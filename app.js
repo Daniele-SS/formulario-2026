ea61014568ee92ebbb34e36bee2ed63ad5220b71
Daniele-SS <dev.daniele@outlook.com>
2026-05-04 08:50:05 -0300

    feat: adiciona nova URL para a API brasil

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 //Função assíncrona
 async function getDadosCep(cep) {
-    const url       = `https://viacep.com.br/ws/${cep}/json/`
+    const url       = `https://brasilapi.com.br/api/cep/v2/${cep}`
     const response  = await fetch(url)
     const data      = await response.json()
 
@@ -13,10 +13,10 @@ async function preencherFormulario() {
     const cep = document.getElementById('cep').value
     const dadosCep = await getDadosCep(cep)
     
-    document.getElementById('endereco').value    = dadosCep.logradouro 
-    document.getElementById('bairro').value      = dadosCep.bairro
-    document.getElementById('cidade').value      = dadosCep.localidade
-    document.getElementById('estado').value      = dadosCep.estado 
+    document.getElementById('endereco').value = dadosCep.street 
+    document.getElementById('bairro').value   = dadosCep.neighborhood
+    document.getElementById('cidade').value   = dadosCep.city
+    document.getElementById('estado').value   = dadosCep.state 
 }
 
 document.getElementById('cep').addEventListener('focusout', preencherFormulario)
